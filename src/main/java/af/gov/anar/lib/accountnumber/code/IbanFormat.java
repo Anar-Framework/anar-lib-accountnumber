75b4b71f306ab49eb5f14755847032b296ffed68
mohbadar <mohammadbadarhashimi@gmail.com>
2020-04-25 10:16:59 +0430

    iban format

---
+++ src/main/java/af/gov/anar/lib/accountnumber/code/IbanFormat.java
@@ -0,0 +1,17 @@
+package af.gov.anar.lib.accountnumber.code;
+
+/**
+ * Iban Format Enum
+ */
+public enum IbanFormat {
+    /**
+     * Default Iban Format.
+     * Groups of four characters separated by a single space.
+     */
+    Default,
+
+    /**
+     * No Format.
+     */
+    None
+}
