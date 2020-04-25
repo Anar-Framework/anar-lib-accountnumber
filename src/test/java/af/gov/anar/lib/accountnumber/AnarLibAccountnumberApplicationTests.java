d8ee58ce18615ea6b199df7bb4268e035c62d330
mohbadar <mohammadbadarhashimi@gmail.com>
2020-04-25 09:55:04 +0430

    account number generator library of Anar Framework

---
+++ src/test/java/af/gov/anar/lib/accountnumber/AnarLibAccountnumberApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.accountnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibAccountnumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
